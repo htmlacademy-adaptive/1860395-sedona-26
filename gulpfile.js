487487b288dea696bf9f926742b1d0ecaf30ff75
Valentina <Elbereth2920@mail.ru>
2022-09-18 14:30:25 +0300

    исправляет автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ const createWebp = () => {
 // SVG
 
 const svg = () =>
-  gulp.src(['source/img/*.svg', '!source/img/icons*.svg'])
+  gulp.src(['source/img/**/*.svg', '!source/img/icons*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'));
 
@@ -88,8 +88,8 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff2,woff}',
     'source/*.ico',
+    'source/*.webmanifest'
   ],
-    ('manifest.webmanifest'),
     {
     base: 'source'
   })
